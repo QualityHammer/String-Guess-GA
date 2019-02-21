26b2620d6235ddba6d68e7012d222b9c989907d2
Ryan <agingllama@gmail.com>
2019-02-20 19:47:58 -0500

    Added a reset button and an input for the mutation rate.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,27 @@
 let population;
-var bestP, fitP, currentGen;
+var target;
+var bestP, fitP, currentGen, mRate, resetB;
 
 // returns a string with a replaced value
 function replaceAt(string, index, replace) {
   return string.substring(0, index) + replace + string.substring(index + 1);
 }
 
+// is called when reset button is pressed
+function resetPop() {
+  population = new Population(target);
+  population.newPop();
+  population.calcFitness();
+}
+
+// is called when the mutation rate is changed
+function mutationChange() {
+  population.mRate = this.value();
+}
+
 function setup() {
   // population creation
-  var target = "The quick brown fox jumped over the lazy dog."
+  target = "The quick brown fox jumped over the lazy dog."
   population = new Population(target);
   population.newPop();
   population.calcFitness();
@@ -16,8 +29,14 @@ function setup() {
   // html
   createElement('h1', target);
   currentGen = createElement('h2', 'Generation: ' + population.generations);
-  fitP = createP('');
+  fitP = createP('Fitness: ');
   bestP = createP('');
+
+  // controls
+  resetB = createButton('Reset');
+  resetB.mousePressed(resetPop);
+  mRate = createInput(population.mRate);
+  mRate.input(mutationChange);
 }
 
 function draw() {
@@ -29,7 +48,7 @@ function draw() {
     // calculate fitness
     population.calcFitness();
     bestP.html(population.getBest().genes);
-    fitP.html(population.getBest().fitness);
+    fitP.html('Fitness: ' + population.getBest().fitness);
   }
   currentGen.html('Generation: ' + population.generations);
 }
